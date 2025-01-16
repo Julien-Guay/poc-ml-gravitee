6df790c21766315ea149861d37772ca723c7b4a2
jguay <julien.guay@insee.fr>
2025-01-16 15:59:58 +0000

    Update model params

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # poc-ml-gravitee
 Deployment of ML models behind a gravitee API manager
+
+### Running API
+```
+uvicorn app.main:app --reload&
+```
--- app/main.py
+++ app/main.py
@@ -3,32 +3,18 @@ from pydantic import BaseModel
 import joblib
 import numpy as np
 
-# Charger le modèle ML pré-entraîné
+# Loading the model
 MODEL_PATH = "models/linear_model.pkl"
 model = joblib.load(MODEL_PATH)
 
-# Créer l'application FastAPI
 app = FastAPI()
 
-# Définir le format des requêtes en utilisant Pydantic
-class PredictionRequest(BaseModel):
-    feature1: float
-    feature2: float
-    feature3: float
-
-# Endpoint pour vérifier le statut de l'API
 @app.get("/")
 def read_root():
-    return {"message": "L'API est en ligne et fonctionne"}
+    return {"message": "API OK"}
 
-# Endpoint pour effectuer une prédiction
-@app.post("/predict")
-def predict(request: PredictionRequest):
-    # Préparer les données d'entrée pour le modèle
-    input_data = np.array([[request.feature1, request.feature2, request.feature3]])
-    
-    # Faire une prédiction
+@app.get("/predict")
+def predict(feature: float):
+    input_data = np.array([[feature]])
     prediction = model.predict(input_data)
-    
-    # Retourner la prédiction sous forme JSON
     return {"prediction": prediction[0]}
